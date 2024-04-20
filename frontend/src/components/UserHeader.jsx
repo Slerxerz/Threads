e35ca6bc40c65e1a97f90be4a5f42417f584a2db
Slerxerz <slerxerz@gmail.com>
2024-04-20 21:58:41 +0545

    responsiveness to profile image

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -31,7 +31,10 @@ const UserHeader = () => {
                 </Flex>
             </Box>
             <Box>
-                <Avatar name="Mark Zuckerberg" src="/zuck-avatar.png" size={"xl"}/>
+                <Avatar name="Mark Zuckerberg" src="/zuck-avatar.png" size={{
+                    base:"lg",
+                    md:"xl",
+                }}/>
             </Box>
         </Flex>
         <Text>Co-founder, executive chairman and CEO of Meta Platform</Text>
--- frontend/src/pages/userpage.jsx
+++ frontend/src/pages/userpage.jsx
@@ -6,7 +6,7 @@ const UserPage = ()=>{
         <>
         <UserHeader/>
         <UserPost likes={1200} replies={481} postImg="/post1.png" postTitle="Let's talk about threads."/>
-        <UserPost likes={3400} replies={51} postImg="/post2.png" postTitle="Nice Tutorial !!!"/>
+        <UserPost likes={3400} replies={51} postImg="/post2.png" postTitle="Nice Tutorial!!!"/>
         <UserPost likes={124} replies={131} postImg="/post3.png" postTitle="I love this guy."/>
         <UserPost likes={23} replies={3} postTitle="This is my first thread."/>
 
